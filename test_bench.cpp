c51ab49f0c9cab9372e6e1977dcb93288ef01db3
murata <mmurata@rcnp.osaka-u.ac.jp>
2022-10-28 22:28:25 +0900

    Add: Decode routine in test_bench.cpp

--- test_bench.cpp
+++ test_bench.cpp
@@ -8,6 +8,7 @@
 #include <functional>
 #include <future>
 #include <regex>
+#include <utility>
 
 // #include <libpq-fe.h>
 #include <pqxx/pqxx>
@@ -26,7 +27,8 @@ std::string MakeAA(const std::vector<T> &_vals,
                    const std::function<double(T, unsigned int, unsigned int)> _funcX,
                    const std::function<double(T, unsigned int, unsigned int)> _funcY,
                    unsigned int _xDiv, double _xMin, double _xMax,
-                   unsigned int _yDiv, double _yMin, double _yMax)
+                   unsigned int _yDiv, double _yMin, double _yMax,
+                   std::string _title = "")
 {
     const unsigned int nVals = _vals.size();
     std::vector<std::pair<double, double>> xys;
@@ -40,7 +42,7 @@ std::string MakeAA(const std::vector<T> &_vals,
     if (_xMin >= _xMax || _yMin >= _yMax)
         return "";
 
-    std::vector<std::vector<unsigned int>> histogram(_yDiv, std::vector<unsigned int>(_xDiv, 0));
+    std::vector<std::vector<unsigned int>> histogram(_xDiv, std::vector<unsigned int>(_yDiv, 0));
     // Fill histogram
     std::for_each(xys.begin(), xys.end(),
                   [&](auto _xy)
@@ -57,17 +59,49 @@ std::string MakeAA(const std::vector<T> &_vals,
                       }
                   });
 
+    uint32_t aaWidth = _xDiv + 2;
     std::ostringstream tmp;
+    tmp << std::setw(aaWidth) << _title.substr(0, aaWidth) << std::endl;
+    {
+        std::ostringstream tmpX;
+        tmpX << "X : [" << _xMin << " -- " << _xMax << "]";
+        tmp << std::setw(aaWidth) << tmpX.str().substr(0, aaWidth) << std::endl;
+    }
+    {
+        std::ostringstream tmpY;
+        tmpY << "Y : [" << _yMin << " -- " << _yMax << "]";
+        tmp << std::setw(aaWidth) << tmpY.str().substr(0, aaWidth) << std::endl;
+    }
+    for (auto i = 0; i < (int)_xDiv + 2; ++i)
+    {
+        tmp << "-";
+    }
+    tmp << std::endl;
 
     for (int iY = _yDiv - 1; iY >= 0; --iY)
     {
+        tmp << "|";
         for (int iX = 0; iX < (int)_xDiv; ++iX)
         {
             // std::cout << iX << " " << iY << std::endl;
-            tmp << ((histogram.at(iX).at(iY) > 0) ? 'X' : ' ');
+            try
+            {
+                tmp << ((histogram.at(iX).at(iY) > 0) ? 'X' : ' ');
+            }
+            catch (std::exception &e)
+            {
+                std::cerr << e.what() << std::endl;
+            }
         }
+        tmp << "|";
         tmp << std::endl;
     }
+
+    for (auto i = 0; i < (int)_xDiv + 2; ++i)
+    {
+        tmp << "-";
+    }
+    tmp << std::endl;
     return tmp.str();
 }
 
@@ -78,9 +112,415 @@ struct ResultsOfThread
     std::vector<MAIKo2Decoder::RawFilesRecord> files;
 };
 
+struct EventIndex
+{
+    uint32_t run_id;
+    uint32_t plane_id;
+    uint32_t board_id;
+    std::string file_path;
+    uint32_t event_data_address;
+    uint32_t event_data_length;
+    uint32_t event_fadc_words_offset;
+    uint32_t event_tpc_words_offset;
+
+    std::string Dump() const
+    {
+        std::ostringstream tmp;
+        tmp << "run_id                  : " << run_id << std::endl;
+        tmp << "plane_id                : " << plane_id << std::endl;
+        tmp << "board_id                : " << board_id << std::endl;
+        tmp << "file_path               : " << file_path << std::endl;
+        tmp << "event_data_address      : " << event_data_address << std::endl;
+        tmp << "event_data_length       : " << event_data_length << std::endl;
+        tmp << "event_fadc_words_offset : " << event_fadc_words_offset << std::endl;
+        tmp << "event_tpc_words_offset  : " << event_tpc_words_offset << std::endl;
+
+        return tmp.str();
+    }
+};
+
+struct FragmentedEventData
+{
+    uint32_t run_id;
+    uint32_t plane_id;
+    uint32_t board_id;
+    uint32_t event_number;
+    MAIKo2Decoder::CounterData counter;
+    MAIKo2Decoder::FADCData fadc;
+    MAIKo2Decoder::TPCData tpc;
+};
+
+class BuiltEventData
+{
+public:
+    using Hit = MAIKo2Decoder::TPCData::Hit;
+    using ShortWordType = MAIKo2Decoder::FADCData::ShortWordType;
+
+    BuiltEventData()
+        : fHitMapper([](uint32_t _plane_id, uint32_t _board_id, Hit _hit) -> Hit
+                     { return {_hit.strip + _board_id * 128, _hit.clock}; }),
+          fFADCChMapper([](uint32_t _plane_id, uint32_t _board_id, uint32_t _ch) -> uint32_t
+                        { return _ch + _board_id * MAIKo2Decoder::FADCData::NumberOfChannels; }){};
+
+    BuiltEventData(std::function<Hit(uint32_t, uint32_t, Hit)> _fHitMapper,
+                   std::function<uint32_t(uint32_t, uint32_t, uint32_t)> _fFADCChMapper)
+        : fHitMapper(_fHitMapper),
+          fFADCChMapper(_fFADCChMapper){};
+
+    struct AddFragmentResult
+    {
+        AddFragmentResult()
+            : good(false), fragment_key_duplication(false), map_duplication(false){};
+        bool good;
+        bool fragment_key_duplication;
+        bool map_duplication;
+    };
+
+    AddFragmentResult AddFragment(const FragmentedEventData &_frg)
+    {
+        AddFragmentResult result;
+        // Check duplication
+        // Duplication of fragment key
+        if (fEventFragments.count({_frg.plane_id, _frg.board_id}) > 0)
+        {
+            result.good = false;
+            result.fragment_key_duplication = true;
+            return result;
+        }
+        for (uint32_t iCh = 0; iCh < MAIKo2Decoder::FADCData::NumberOfChannels; ++iCh)
+        {
+            uint32_t chMapped = fFADCChMapper(_frg.plane_id, _frg.board_id, iCh);
+            // Duplication in inverted map is detected
+            if (fFADCInvertedMap.count({_frg.plane_id, chMapped}) > 0)
+            {
+                result.good = false;
+                result.map_duplication = true;
+                return result;
+            }
+        }
+
+        // No duplication detected -> Add
+        for (uint32_t iCh = 0; iCh < MAIKo2Decoder::FADCData::NumberOfChannels; ++iCh)
+        {
+            uint32_t chMapped = fFADCChMapper(_frg.plane_id, _frg.board_id, iCh);
+            KeyOfFragment keyFragment(_frg.plane_id, _frg.board_id);
+            // Register into inv-Map
+            auto fragment_locator = std::make_pair(keyFragment, iCh);
+            fFADCInvertedMap.insert(std::make_pair(FullyQualifiedChannelForFADC(_frg.plane_id, chMapped), fragment_locator));
+            // Register into Fragments
+            fEventFragments.insert(std::make_pair(keyFragment, _frg));
+        }
+        result.good = true;
+        return result;
+    };
+
+    std::vector<Hit> GetHits(uint32_t _plane_id) const
+    {
+
+        std::vector<Hit> ret;
+        for (auto &frg : fEventFragments)
+        {
+            if (frg.first.plane_id != _plane_id)
+                continue;
+
+            for (auto hit : frg.second.tpc.GetHits())
+            {
+                ret.emplace_back(fHitMapper(_plane_id, frg.second.board_id, hit));
+
+                // std::cout << "Plane " << _plane_id
+                //           << " board " << frg.second.board_id
+                //           << " hit " << hit.strip << " " << hit.clock << std::endl;
+                // std::cout << "    -> "
+                //           << fHitMapper(_plane_id, frg.second.board_id, hit).strip << " "
+                //           << fHitMapper(_plane_id, frg.second.board_id, hit).clock
+                //           << std::endl;
+            }
+        }
+        return ret;
+    }
+
+    std::vector<ShortWordType> GetSignal(uint32_t _plane_id, uint32_t _ch) const
+    {
+        auto itMap = fFADCInvertedMap.find({_plane_id, _ch});
+        if (itMap == fFADCInvertedMap.end())
+            return {};
+
+        auto keyOfFragment = itMap->second.first;
+        uint32_t ch = itMap->second.second;
+
+        return fEventFragments.at(keyOfFragment).fadc.GetSignal(ch);
+    }
+
+    std::vector<uint32_t> GetAvailableFADCCh(uint32_t _plane_id) const
+    {
+        std::vector<uint32_t> ret;
+        std::for_each(fFADCInvertedMap.begin(), fFADCInvertedMap.end(),
+                      [&](auto &_item)
+                      {
+                          if (_item.first.plane_id == _plane_id)
+                              ret.push_back(_item.first.ch);
+                      });
+        std::sort(ret.begin(), ret.end(), std::less<uint32_t>());
+        return ret;
+    }
+
+private:
+    // Mapper function for TPC Hit (plane_id, board_id, Hit) -> Hit (mapped)
+    std::function<Hit(uint32_t, uint32_t, Hit)> fHitMapper;
+
+    // Mapper function for FADC Ch (plane_id, board_id, ch) -> ch (mapped)
+    std::function<uint32_t(uint32_t, uint32_t, uint32_t)> fFADCChMapper;
+
+    struct FullyQualifiedChannelForFADC
+    {
+        FullyQualifiedChannelForFADC(uint32_t _plane_id, uint32_t _ch)
+            : plane_id(_plane_id), ch(_ch){};
+        uint32_t plane_id;
+        uint32_t ch;
+        bool operator<(const FullyQualifiedChannelForFADC &_rhs) const
+        {
+            if (plane_id != _rhs.plane_id)
+                return plane_id < _rhs.plane_id;
+            else if (ch != _rhs.ch)
+                return ch < _rhs.ch;
+            else
+                return false;
+        }
+    };
+
+    struct KeyOfFragment
+    {
+        uint32_t plane_id;
+        uint32_t board_id;
+        KeyOfFragment(uint32_t _plane_id, uint32_t _board_id)
+            : plane_id(_plane_id), board_id(_board_id){};
+        bool operator<(const KeyOfFragment &_rhs) const
+        {
+            if (plane_id != _rhs.plane_id)
+                return plane_id < _rhs.plane_id;
+            else if (board_id != _rhs.board_id)
+                return board_id < _rhs.board_id;
+            else
+                return false;
+        }
+    };
+
+    // Inverted map for fetching FADC signal efficiently
+    std::map<FullyQualifiedChannelForFADC, std::pair<KeyOfFragment, uint32_t>> fFADCInvertedMap;
+
+    // Fragment store
+    std::map<KeyOfFragment, FragmentedEventData> fEventFragments;
+
+    // uint32_t a;
+};
+
 int main(int argc, char *argv[])
 {
 
+    if (argc < 3)
+    {
+        std::cerr << "[Usage] : " << argv[0] << " [run_id] [event_number]" << std::endl;
+        return 1;
+    }
+
+    uint32_t run_id = atoi(argv[1]);
+
+    // Identifier of the event
+    //    Same to trigger_counter for now.
+    uint32_t event_number = atoi(argv[2]);
+
+    // Connect to db
+    pqxx::connection c("");
+    std::cout << "Connected to " << c.dbname() << '\n';
+
+    pqxx::work tx{c};
+    std::vector<EventIndex> vIndexes;
+    try
+    {
+        std::ostringstream query;
+        query << "SELECT "
+              << "e.run_id, e.plane_id, e.board_id, f.file_path, "
+              << "e.event_data_address, e.event_data_length, "
+              << "e.event_fadc_words_offset, e.event_tpc_words_offset "
+              << "FROM test.raw_events AS e "
+              << "INNER JOIN test.raw_files AS f ON "
+              << "e.run_id = f.run_id AND "
+              << "e.plane_id = f.plane_id AND "
+              << "e.board_id = f.board_id AND "
+              << "e.file_number = f.file_number "
+              << "WHERE "
+              << "e.run_id = " << run_id << " AND "
+              << "e.event_trigger_counter = " << event_number << " "
+              << "ORDER BY (e.plane_id, e.board_id)"
+              << ";"
+              << std::endl;
+
+        pqxx::result res(tx.exec(query.str()));
+
+        for (auto row : res)
+        {
+            for (auto i = 0; i < (int)row.size(); ++i)
+            {
+                std::cout << row[i] << " ";
+            }
+            std::cout << std::endl;
+
+            EventIndex ind;
+            ind.run_id = row[0].as<decltype(ind.run_id)>();
+            ind.plane_id = row[1].as<decltype(ind.plane_id)>();
+            ind.board_id = row[2].as<decltype(ind.board_id)>();
+            ind.file_path = row[3].as<decltype(ind.file_path)>();
+            ind.event_data_address = row[4].as<decltype(ind.event_data_address)>();
+            ind.event_data_length = row[5].as<decltype(ind.event_data_length)>();
+            ind.event_fadc_words_offset = row[6].as<decltype(ind.event_fadc_words_offset)>();
+            ind.event_tpc_words_offset = row[7].as<decltype(ind.event_tpc_words_offset)>();
+            vIndexes.push_back(ind);
+        }
+
+        tx.commit();
+    }
+    catch (const pqxx::sql_error &_e)
+    {
+        std::cerr << "[Error] : SQL exception occurred while selecting event records for "
+                  << "run " << run_id << ", event number " << event_number << " "
+                  << "from DB." << std::endl;
+        std::cerr << _e.what() << std::endl;
+    }
+    catch (const pqxx::usage_error &_e)
+    {
+        std::cerr << "[Error] : Some libpqxx usage exception occurred while selecting event records for "
+                  << "run " << run_id << ", event number " << event_number << " "
+                  << "from DB." << std::endl;
+        std::cerr << _e.what() << std::endl;
+    }
+    catch (const std::exception &_e)
+    {
+        std::cerr << "[Error] : Some exception occurred while selecting event records for "
+                  << "run " << run_id << ", event number " << event_number << " "
+                  << "from DB." << std::endl;
+        std::cerr << _e.what() << std::endl;
+    }
+
+    for (auto &ind : vIndexes)
+    {
+        std::cout << ind.Dump() << std::endl;
+    }
+
+    if (vIndexes.size() == 0)
+    {
+        std::cerr << "[Error] : Run " << run_id << ", event " << event_number << " is NOT found." << std::endl;
+        return 1;
+    }
+
+    // Decode
+    std::vector<FragmentedEventData> vFragments;
+    for (auto &ind : vIndexes)
+    {
+        std::ifstream fIn(ind.file_path, std::ios::binary);
+        if (!fIn.good())
+        {
+            std::cerr << "[Error] : File " << ind.file_path << " is NOT found." << std::endl;
+            return 1;
+        }
+        fIn.seekg(ind.event_data_address, std::ios_base::beg);
+        auto wordsEvent = MAIKo2Decoder::ReadNextWords(fIn, ind.event_data_length / sizeof(MAIKo2Decoder::WordType));
+        MAIKo2Decoder::EventWordsBuffer buf(wordsEvent, ind.event_fadc_words_offset, ind.event_tpc_words_offset);
+
+        if (!buf.IsValid())
+        {
+            std::cerr << "[Error] : Word buffer at " << ind.event_data_address << " "
+                      << "in the file " << ind.file_path << " "
+                      << "is in the wrong format." << std::endl;
+            return 1;
+        }
+
+        MAIKo2Decoder::CounterData counter(buf.GetCounterWords());
+        MAIKo2Decoder::FADCData fadc(buf.GetFADCWords());
+        MAIKo2Decoder::TPCData tpc(buf.GetTPCWords());
+
+        FragmentedEventData frg;
+        frg.run_id = ind.run_id;
+        frg.plane_id = ind.plane_id;
+        frg.board_id = ind.board_id;
+        frg.event_number = event_number;
+        frg.counter = counter;
+        frg.fadc = fadc;
+        frg.tpc = tpc;
+
+        vFragments.push_back(frg);
+
+        if (!counter.IsGood() ||
+            !fadc.IsGood() ||
+            !tpc.IsGood())
+        {
+            std::cerr << "[Error] : Decode for the event at " << ind.event_data_address << " "
+                      << "in the file " << ind.file_path << " "
+                      << "failed." << std::endl;
+
+            std::cerr << counter.IsGood() << " "
+                      << fadc.IsGood() << " "
+                      << tpc.IsGood() << std::endl;
+
+            return 1;
+        }
+    }
+
+    BuiltEventData data;
+    for (auto &frg : vFragments)
+    {
+        auto result = data.AddFragment(frg);
+        // std::cout << frg.plane_id << " " << frg.board_id << std::endl;
+        if (!result.good)
+        {
+            std::cerr << "[Error] : Build for the event fragment with the plane_id " << frg.plane_id << " "
+                      << "and the board_id " << frg.board_id << " "
+                      << "failed." << std::endl;
+            std::cerr << result.fragment_key_duplication << " " << result.map_duplication << std::endl;
+        }
+    }
+
+    auto hitsA = data.GetHits(0);
+    auto hitsC = data.GetHits(1);
+
+    using Hit = MAIKo2Decoder::TPCData::Hit;
+    auto aaA = MakeAA(
+        hitsA,
+        std::function<double(Hit, unsigned int, unsigned int)>([](Hit _hit, unsigned int, unsigned int) -> double
+                                                               { return (double)_hit.strip; }),
+        std::function<double(Hit, unsigned int, unsigned int)>([](Hit _hit, unsigned int, unsigned int) -> double
+                                                               { return (double)_hit.clock; }),
+        // [](TPCData::Hit _hit, unsigned int, unsigned int) -> double
+        // { return (double)_hit.clock; },
+        64u, 0., 768,
+        32u, 0., 2048.,
+        "Anode");
+
+    auto aaC = MakeAA(
+        hitsC,
+        std::function<double(Hit, unsigned int, unsigned int)>([](Hit _hit, unsigned int, unsigned int) -> double
+                                                               { return (double)_hit.strip; }),
+        std::function<double(Hit, unsigned int, unsigned int)>([](Hit _hit, unsigned int, unsigned int) -> double
+                                                               { return (double)_hit.clock; }),
+        // [](TPCData::Hit _hit, unsigned int, unsigned int) -> double
+        // { return (double)_hit.clock; },
+        64u, 0., 768,
+        32u, 0., 2048.,
+        "Cathode");
+
+    std::istringstream isA(aaA);
+    std::istringstream isC(aaC);
+
+    std::string lineA, lineC;
+    while (std::getline(isA, lineA) && std::getline(isC, lineC))
+    {
+        std::cout << lineA << "    " << lineC << std::endl;
+    }
+    // std::cout << aaA << std::endl;
+
+    // std::cout << "==========" << std::endl;
+    // std::cout << aaC << std::endl;
+
+#if 0
     pqxx::connection c;
     std::cout << "Connected to " << c.dbname() << '\n';
     pqxx::work tx{c};
@@ -552,4 +992,5 @@ int main(int argc, char *argv[])
     std::cout << "Event count " << count << std::endl;
 
     return 0;
+#endif
 }
